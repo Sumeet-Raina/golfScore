fd0d5430a7f388bf1352496e3e137acbc59b3208
sumeet978 <searchsumeet@gmail.com>
2019-12-15 14:44:48 +0000

    Added function golfScore that takes two parameters

--- score.js
+++ score.js
@@ -1,1 +1,5 @@
 var names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+//Added function golfScore that takes two parameters
+function golfScore(par, strokes) { 
+  
+}
