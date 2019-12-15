db2f4da4f51930e60f8528fc468ca72070647158
sumeet978 <searchsumeet@gmail.com>
2019-12-15 14:46:34 +0000

    Added 3 and 4 condition

--- score.js
+++ score.js
@@ -5,5 +5,9 @@ function golfScore(par, strokes) {
     return names[0];
   } else if (strokes <= par - 2) {
     return names[1];
-  }
+  } else if (strokes == par - 1) {
+    return names[2];
+  } else if (strokes == par) {
+    return names[3];
+  } 
 }
